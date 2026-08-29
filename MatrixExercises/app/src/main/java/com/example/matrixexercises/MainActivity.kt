75195cf13b2aa31374d64933fe22ef06709957b4
DevLuquinha <lucasemmanuelrodrigues737@gmail.com>
2026-08-29 09:16:17 -0300

    The exercise3 of assigment 02 - beautiful matrix

--- MatrixExercises/app/src/main/java/com/example/matrixexercises/MainActivity.kt
+++ MatrixExercises/app/src/main/java/com/example/matrixexercises/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.matrixexercises
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var numberInput : TextView;
+    lateinit var btnVerify : Button;
+    lateinit var matrixText : TextView;
+    lateinit var resultText : TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 //        exercise1();
-        exercise2();
+//        exercise2();
+        exercise3();
     }
 
     fun exercise1(){
@@ -90,6 +98,46 @@ class MainActivity : AppCompatActivity() {
         showMatrix(matrixD, columnSize, rowSize,  "Matrix S");
     }
 
+    fun exercise3(){
+        numberInput = findViewById(R.id.et_number);
+        btnVerify = findViewById(R.id.btn_verify);
+        matrixText = findViewById(R.id.tv_matrix);
+        resultText = findViewById(R.id.tv_result);
+
+        var columnSize = 5;
+        var rowSize = 5;
+
+        var matrix = createMatrix(columnSize, rowSize);
+        populateMatrix(matrix, columnSize, rowSize);
+
+        var matrixMessage = "";
+        for (i in 0 until columnSize){
+            for (j in 0 until rowSize){
+                matrixMessage += if (matrix[i][j] < 10) "[0${matrix[i][j]}] " else "[${matrix[i][j]}] ";
+            }
+
+            matrixMessage += "\n";
+        }
+
+        matrixText.text = matrixMessage;
+
+        btnVerify.setOnClickListener {
+            val rawNumber = numberInput.text.toString();
+            if (rawNumber.isNullOrEmpty()){
+                resultText.text = "Error! Type a number to check :(";
+            } else {
+                val targetNumber = rawNumber.toIntOrNull() ?: 0;
+
+                val matrixContainsNumber = containsInMatrix(matrix, targetNumber)
+                if (matrixContainsNumber){
+                    resultText.text = "The $targetNumber already exists in the Matrix";
+                } else {
+                    resultText.text = "The $targetNumber doesn't exist in the Matrix";
+                }
+            }
+        }
+    }
+
     fun createMatrix(columnLength : Int, rowsLength: Int) : Array<IntArray> {
         var matrix = Array(columnLength, { IntArray(rowsLength) });
 
@@ -122,5 +170,17 @@ class MainActivity : AppCompatActivity() {
         }
         Log.i("DEBUG-TEST", matrixMessage);
         Log.i("DEBUG-TEST", "-------------------------------------------");
+    }
+
+    fun containsInMatrix(matrix : Array<IntArray>, targetValue : Int) : Boolean{
+        for (row in matrix){
+            for (number in row){
+                if (number == targetValue){
+                    return true;
+                }
+            }
+        }
+
+        return false; // 404 not found
     }
 }
